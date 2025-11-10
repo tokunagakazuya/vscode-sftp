d63df05c92760dc814ef180bbb37d00340fe4f82
Bruno Chabrier <bruno_chabrier@hotmail.com>
2025-11-10 09:17:15 +0100

    Fix: improved test framework - improved vscode mock to support running tests through Launch Tests as well as npm - improved logging to debug console to display links to file in logs - added FSWatcher emulation to support tests including the file watcher

--- __mocks__/vscode.js
+++ __mocks__/vscode.js
@@ -1,5 +1,7 @@
 // use original console log implementation to avoid jest decorations
 const log = require('console').log.bind(console);
+const { vol } = require('memfs');
+const path = require('path');
 
 const Nothing = (() => {
 	const fn = () => Nothing
@@ -16,10 +18,12 @@ const Nothing = (() => {
 // otherwise, vscode is mocked as Nothing
 module.exports = global.vscode || Nothing;
 
+const vscode = module.exports;
+
 // Mock vscode OutputChannel to include source location in log messages and display in console during tests
 // OutputChannel is normally not visible during tests, making it hard to trace log origins
 // it is used by logger.ts via output.ts
-module.exports.window.createOutputChannel = () => {
+vscode.window.createOutputChannel = () => {
 	return {
 		appendLine: (msg) => {
 			// Error stack in the form:
@@ -38,11 +42,18 @@ module.exports.window.createOutputChannel = () => {
 			let LOCATION_LINE_INDEX = 5;
 			const locationLine = (new Error).stack?.split('\n')[LOCATION_LINE_INDEX];
 
+			// get workspace dir to transform paths to relative
+			// this will allow displaying shorter paths and have vscode debug console highlight links to the files
+			const workspaceDir = __dirname + '/..'; // we know we are in <workspace_dir>/__mocks__
+
 			// line in the form:
 			//  at TransferTask.<anonymous> (C:\Users\bruno\Projects\vscode-sftp-2\src\core\transferTask.ts:160:34)
 			// or
 			//  at C:\Users\bruno\Projects\vscode-sftp-2\src\fileHandlers\transfer\transfer.ts:129:34
-			const location = locationLine ? locationLine.split(/[\\:]/).slice(-3, -2).join(':') : ''; // do not take line number as not accurate (from compiled JS?)
+			const location = '.' + path.sep + path.relative(workspaceDir, locationLine && locationLine.includes('(')
+				? locationLine.replace(/^[^(]+\((.*)\)$/, '$1').replaceAll('/', '\\')
+				: locationLine.replace(/^ +at +/, '')
+			);
 			const f = locationLine && locationLine.includes('(') ? locationLine.split(/ +/).slice(2, 3) : '';
 
 			const msgs = msg.split(' ');
@@ -51,3 +62,80 @@ module.exports.window.createOutputChannel = () => {
 		}
 	}
 }
+
+if (!global.vscode) {
+	// we are running outside of vscode, hence we need to emulate some missing functions
+
+	vscode.Uri.file = (fsPath) => ({
+		scheme: 'file',
+		path: fsPath,
+		fsPath: fsPath,
+		query: '',
+		fragment: '',
+		toString: () => fsPath,
+	});
+
+	class RelativePattern {
+		constructor(base, pattern) {
+			this.base = base;
+			this.pattern = pattern;
+		}
+	}
+	vscode.RelativePattern = RelativePattern.prototype.constructor;
+}
+
+class mockedFSWatcher {
+	createListener;
+	changeListener;
+	deleteListener;
+	watcher;
+
+	constructor(globPattern, ignoreCreateEvents, ignoreChangeEvents, ignoreDeleteEvents) {
+		const root = globPattern.base.replace(/\\/g, '/');
+		this.watcher = vol.watch(root, { persistent: false, recursive: true }, (eventType, filename) => {
+
+			const fsPath = root + '/' + filename;
+			const uri = vscode.Uri.file(fsPath);
+
+			if (eventType === 'rename') {
+				if (!vol.existsSync(uri.fsPath)) {
+					if (!ignoreDeleteEvents) this.deleteListener(uri);
+				} else {
+					// we cannot detect if it's a creation or a change, but anyway, in the app we bind the change and create events
+					// to the same callback, so...
+					if (!ignoreChangeEvents) this.changeListener(uri);
+				}
+			}
+		});
+	}
+
+	onDidCreate(fn) {
+		this.createListener = fn;
+	}
+	onDidChange(fn) {
+		this.changeListener = fn;
+	}
+	onDidDelete(fn) {
+		this.deleteListener = fn;
+	}
+	dispose() {
+		this.watcher.close();
+	}
+
+}
+
+/** @type {(globPattern: GlobPattern, ignoreCreateEvents?: boolean, ignoreChangeEvents?: boolean, ignoreDeleteEvents?: boolean): FileSystemWatcher} */
+const createFileSystemWatcher = (globPattern, ignoreCreateEvents, ignoreChangeEvents, ignoreDeleteEvents) => {
+	const watcher = new mockedFSWatcher(globPattern, ignoreCreateEvents, ignoreChangeEvents, ignoreDeleteEvents);
+
+	return watcher;
+
+};
+
+vscode.workspace.createFileSystemWatcher = createFileSystemWatcher;
+
+// side effect: add fake remoteExplorer to app
+const app = require('../src/app');
+app.default.remoteExplorer = {
+	refresh: () => { }
+};
