e8224b60ec356df0f27bd7a93aa3e2d20fd92753
Bruno Chabrier <bruno_chabrier@hotmail.com>
2025-11-10 22:10:25 +0100

    Improved colors in jtest results during Launch Tests: colors where not displayed in diff parts

--- test/jest-test-runner.ts
+++ test/jest-test-runner.ts
@@ -6,6 +6,8 @@ import * as sourceMapSupport from 'source-map-support';
 import { runCLI } from 'jest';
 import console from 'console';
 import { readConfigs } from 'jest-config';
+import { BaseReporter } from '@jest/reporters';
+import chalk from 'chalk';
 
 
 // fix __dirname issue, cf. https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
@@ -25,25 +27,23 @@ type Argv = Parameters<typeof runCLI>[0];
 const jestConfig = {
     rootDir: rootDir,
     verbose: true,
-    colors: false,
-    reporters: // used with FORCE_COLORS="true", restore the "normal" colors in Debug Console
-        [
-            '<rootDir>/test/jest-restore-colors-verbose-reporter.ts',
-            "<rootDir>/test/jest-restore-colors-summary-reporter.ts"
-        ],
     runInBand: true, // Required due to the way the "vscode" module is injected.
     testEnvironment: fromRoot('test/jest-vscode-environment.ts'),
     watchAll: true,
 } as Argv;
-
-// force colors in chalk (through module supports-color). Must be used together with a customer reporter to restore "normal" colors in Debug Console
-process.env.FORCE_COLOR = "true";
 
 export async function run(_testRoot: string, callback: TestRunnerCallback) {
     // Enable source map support. This is done in the original Mocha test runner,
     // so do it here. It is not clear if this is having any effect.
     sourceMapSupport.install();
 
+    // color display of jest results:
+    // 1/ the Launch Tests task defines the env variable FORCE_COLOR=true as per https://jestjs.io/docs/cli#--colors
+    // 2/ here we patch the reporters' log to override debug console's default color with white 
+    BaseReporter.prototype.log = function log(message: string) {
+        process.stdout.write(`${chalk.white(`${message}`)}\n`);
+    }
+    
     try {
         // get the globalConfig (from package.json)
         const { globalConfig } = await readConfigs(jestConfig, [rootDir]);
